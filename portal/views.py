be8c7eb665a05fe1bcba2f72b51474191a8452c5
Arya <aryampatel2001@gmail.com>
2023-11-02 17:18:18 -0700

    tree structure

--- portal/views.py
+++ portal/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New Account Created: {username}")
+            login(request, user)
+            return redirect("portal:dashboard")  # Redirect to a dashboard view after registration
+        else:
+            for msg in form.error_messages:
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
+
+    form = UserCreationForm()
+    return render(request, "portal/register.html", {"form": form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("portal:dashboard")  # Redirect to a dashboard view after login
+            else:
+                messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+
+    form = AuthenticationForm()
+    return render(request, "portal/login.html", {"form": form})
+
+def user_logout(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.") 
+    return redirect("portal:login")
