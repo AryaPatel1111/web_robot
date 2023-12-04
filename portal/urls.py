57e28861fd403185240bd3702c04e3e33007cf5b
Arya <aryampatel2001@gmail.com>
2023-12-04 00:34:00 -0700

    Login portal complete

--- portal/urls.py
+++ portal/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
+app_name = 'portal'
+
 urlpatterns = [
-    # Existing paths...
+    path('', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='portal/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
